Yamapps/keyple-android-demo-validation

Implements flavors for plugins : OMAPI / Famoco / Coppernic
Signed-off-by: Youssef Amrani <youssef.amrani@yamapps.fr>

validator-app/src/famoco/kotlin/org/cna/keyple/famoco/validator/di/FamocoReaderRepositoryImpl.kt
@@ -1,0 +1,108 @@
+package org.cna.keyple.famoco.validator.di
+
+import android.app.Activity
+import org.cna.keyple.famoco.validator.reader.IReaderRepository
+import org.eclipse.keyple.core.seproxy.SeProxyService
+import org.eclipse.keyple.core.seproxy.SeReader
+import org.eclipse.keyple.core.seproxy.exception.KeypleException
+import org.eclipse.keyple.core.seproxy.protocol.SeCommonProtocols
+import org.eclipse.keyple.famoco.se.plugin.AndroidFamocoPlugin
+import org.eclipse.keyple.famoco.se.plugin.AndroidFamocoPluginFactory
+import org.eclipse.keyple.famoco.se.plugin.AndroidFamocoReader
+import org.eclipse.keyple.plugin.android.nfc.AndroidNfcPlugin
+import org.eclipse.keyple.plugin.android.nfc.AndroidNfcPluginFactory
+import org.eclipse.keyple.plugin.android.nfc.AndroidNfcProtocolSettings
+import org.eclipse.keyple.plugin.android.nfc.AndroidNfcReader
+import timber.log.Timber
+import javax.inject.Inject
+
+/**
+ *
+ *  created on 21/09/2020
+ *
+ *  @author youssefamrani
+ */
+
+class FamocoReaderRepositoryImpl @Inject constructor() :
+    IReaderRepository {
+
+    override var poReader: SeReader? = null
+    override var samReaders: MutableMap<String, SeReader> = mutableMapOf()
+
+    @Throws(KeypleException::class)
+    override fun registerPlugin() {
+        SeProxyService.getInstance().registerPlugin(AndroidNfcPluginFactory())
+        try {
+            SeProxyService.getInstance().registerPlugin(AndroidFamocoPluginFactory())
+        } catch (e: UnsatisfiedLinkError) {
+            Timber.w(e)
+        }
+    }
+
+    @Throws(KeypleException::class)
+    override suspend fun initPoReader(): SeReader? {
+        val readerPlugin = SeProxyService.getInstance().getPlugin(AndroidNfcPlugin.PLUGIN_NAME)
+        poReader = readerPlugin.readers.values.first()
+
+        poReader?.let {
+            Timber.d("Initialize SEProxy with Android Plugin")
+
+            // define task as an observer for ReaderEvents
+            Timber.d("PO (NFC) reader name: ${it.name}")
+
+            it.setParameter("FLAG_READER_RESET_STATE", "0")
+            it.setParameter("FLAG_READER_PRESENCE_CHECK_DELAY", "100")
+            it.setParameter("FLAG_READER_NO_PLATFORM_SOUNDS", "0")
+            it.setParameter("FLAG_READER_SKIP_NDEF_CHECK", "0")
+
+            // with this protocol settings we activate the nfc for ISO1443_4 protocol
+            it.addSeProtocolSetting(
+                SeCommonProtocols.PROTOCOL_ISO14443_4,
+                AndroidNfcProtocolSettings.getSetting(SeCommonProtocols.PROTOCOL_ISO14443_4)
+            )
+            it.addSeProtocolSetting(
+                SeCommonProtocols.PROTOCOL_MIFARE_CLASSIC,
+                AndroidNfcProtocolSettings.getSetting(SeCommonProtocols.PROTOCOL_MIFARE_CLASSIC)
+            )
+        }
+
+        return poReader
+    }
+
+    @Throws(KeypleException::class)
+    override suspend fun initSamReaders(): Map<String, SeReader> {
+        if (samReaders.isNullOrEmpty()) {
+            val samPlugin = SeProxyService.getInstance().getPlugin(AndroidFamocoPlugin.PLUGIN_NAME)
+
+            if (samPlugin != null) {
+                val samReader = samPlugin.getReader(AndroidFamocoReader.READER_NAME)
+
+                if (samReader != null) {
+                    samReaders[AndroidFamocoReader.READER_NAME] = samReader
+                }
+            }
+        }
+
+        return samReaders
+    }
+
+    override fun setSamParameters(samReader: SeReader) {
+        samReader.setParameter(AndroidFamocoReader.FLAG_READER_RESET_STATE, "")
+    }
+
+    override fun enableNfcReaderMode(activity: Activity) {
+        (poReader as AndroidNfcReader).enableNFCReaderMode(activity)
+    }
+
+    override fun disableNfcReaderMode(activity: Activity) {
+        (poReader as AndroidNfcReader).disableNFCReaderMode(activity)
+    }
+
+    override fun getSamReader(): SeReader? {
+        return samReaders[AndroidFamocoReader.READER_NAME]
+    }
+
+    override fun onDestroy() {
+        //Do nothing
+    }
+}

validator-app/src/main/kotlin/org/cna/keyple/famoco/validator/ticketing/AbstractTicketingSession.kt
@@ -11,15 +11,11 @@
  ********************************************************************************/
 package org.cna.keyple.famoco.validator.ticketing
 
+import org.cna.keyple.famoco.validator.reader.IReaderRepository
 import org.eclipse.keyple.calypso.command.sam.SamRevision
-import org.eclipse.keyple.calypso.transaction.CalypsoPo
-import org.eclipse.keyple.calypso.transaction.CalypsoSam
-import org.eclipse.keyple.calypso.transaction.ElementaryFile
-import org.eclipse.keyple.calypso.transaction.PoSecuritySettings
+import org.eclipse.keyple.calypso.transaction.*
 import org.eclipse.keyple.calypso.transaction.PoSecuritySettings.PoSecuritySettingsBuilder
 import org.eclipse.keyple.calypso.transaction.PoTransaction.SessionSetting.AccessLevel
-import org.eclipse.keyple.calypso.transaction.SamSelectionRequest
-import org.eclipse.keyple.calypso.transaction.SamSelector
 import org.eclipse.keyple.core.selection.SeResource
 import org.eclipse.keyple.core.selection.SeSelection
 import org.eclipse.keyple.core.selection.SelectionsResult
@@ -28,10 +24,12 @@ import org.eclipse.keyple.core.seproxy.SeReader
 import org.eclipse.keyple.core.seproxy.event.ObservableReader
 import org.eclipse.keyple.core.seproxy.exception.KeypleReaderException
 import org.eclipse.keyple.core.seproxy.protocol.SeCommonProtocols
-import org.eclipse.keyple.famoco.se.plugin.AndroidFamocoReader
 import timber.log.Timber
 
-abstract class AbstractTicketingSession protected constructor(val poReader: SeReader, protected val samReader: SeReader?) {
+abstract class AbstractTicketingSession protected constructor(
+    protected val readerRepository: IReaderRepository
+) {
+
     protected lateinit var calypsoPo: CalypsoPo
     protected lateinit var seSelection: SeSelection
     var poTypeName: String? = null
@@ -44,6 +42,7 @@ abstract class AbstractTicketingSession protected constructor(val poReader: SeRe
     protected lateinit var efEventLog: ElementaryFile
     protected lateinit var efCounter: ElementaryFile
     protected lateinit var efContractParser: ElementaryFile
+
     protected fun pad(text: String, c: Char, length: Int): String {
         val sb = StringBuffer(length)
         sb.append(text)
@@ -94,19 +93,20 @@ abstract class AbstractTicketingSession protected constructor(val poReader: SeRe
     }
 
     fun notifySeProcessed() {
-        (poReader as ObservableReader).finalizeSeProcessing()
+        (readerRepository.poReader as ObservableReader).finalizeSeProcessing()
     }
 
     @Throws(KeypleReaderException::class, IllegalStateException::class)
     protected fun checkSamAndOpenChannel(samReader: SeReader): SeResource<CalypsoSam> {
-        samReader.setParameter(AndroidFamocoReader.FLAG_READER_RESET_STATE, "")
+        readerRepository.setSamParameters(samReader)
         /*
          * check the availability of the SAM doing a ATR based selection, open its physical and
          * logical channels and keep it open
          */
         val samSelection = SeSelection(MultiSeRequestProcessing.FIRST_MATCH)
 
-        val samSelector = SamSelector.builder().seProtocol(SeCommonProtocols.PROTOCOL_ISO7816_3).samRevision(SamRevision.C1).build()
+        val samSelector = SamSelector.builder().seProtocol(SeCommonProtocols.PROTOCOL_ISO7816_3)
+            .samRevision(SamRevision.C1).build()
 
         samSelection.prepareSelection(SamSelectionRequest(samSelector))
 
@@ -157,5 +157,9 @@ abstract class AbstractTicketingSession protected constructor(val poReader: SeRe
                 DEFAULT_KEY_RECORD_NUMBER_DEBIT
             )
             .build()
+    }
+
+    fun samReaderAvailable(): Boolean {
+        return readerRepository.getSamReader() != null
     }
 }

validator-app/src/main/kotlin/org/cna/keyple/famoco/validator/util/Extensions.kt
@@ -1,0 +1,20 @@
+package org.cna.keyple.famoco.validator.util
+
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlinx.coroutines.withTimeoutOrNull
+import kotlin.coroutines.Continuation
+
+/**
+ *
+ *  created on 01/10/2020
+ *
+ *  @author youssefamrani
+ */
+
+suspend inline fun <T> suspendCoroutineWithTimeout(timeout: Long, crossinline block: (Continuation<T>) -> Unit ) : T? {
+    var finalValue : T? = null
+    withTimeoutOrNull(timeout) {
+        finalValue = suspendCancellableCoroutine(block = block)
+    }
+    return finalValue
+}

validator-app/src/omapi/kotlin/org/cna/keyple/famoco/validator/di/OmapiReaderRepositoryImpl.kt
@@ -1,0 +1,120 @@
+package org.cna.keyple.famoco.validator.di
+
+import android.app.Activity
+import android.content.Context
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
+import org.cna.keyple.famoco.validator.reader.IReaderRepository
+import org.eclipse.keyple.core.seproxy.SeProxyService
+import org.eclipse.keyple.core.seproxy.SeReader
+import org.eclipse.keyple.core.seproxy.exception.KeypleException
+import org.eclipse.keyple.core.seproxy.exception.KeyplePluginInstantiationException
+import org.eclipse.keyple.core.seproxy.protocol.SeCommonProtocols
+import org.eclipse.keyple.plugin.android.nfc.AndroidNfcPlugin
+import org.eclipse.keyple.plugin.android.nfc.AndroidNfcPluginFactory
+import org.eclipse.keyple.plugin.android.nfc.AndroidNfcProtocolSettings
+import org.eclipse.keyple.plugin.android.nfc.AndroidNfcReader
+import org.eclipse.keyple.plugin.android.omapi.AndroidOmapiPluginFactory
+import org.eclipse.keyple.plugin.android.omapi.PLUGIN_NAME
+import timber.log.Timber
+import javax.inject.Inject
+
+/**
+ *
+ *  created on 21/09/2020
+ *
+ *  @author youssefamrani
+ */
+
+class OmapiReaderRepositoryImpl @Inject constructor(private val applicationContext: Context) :
+    IReaderRepository {
+
+    override var poReader: SeReader? = null
+    override var samReaders: MutableMap<String, SeReader> = mutableMapOf()
+
+    @Throws(KeypleException::class)
+    override fun registerPlugin() {
+        SeProxyService.getInstance().registerPlugin(AndroidNfcPluginFactory())
+        SeProxyService.getInstance().registerPlugin(AndroidOmapiPluginFactory(applicationContext))
+    }
+
+    @Throws(KeypleException::class)
+    override suspend fun initPoReader(): SeReader? {
+        val readerPlugin = SeProxyService.getInstance().getPlugin(AndroidNfcPlugin.PLUGIN_NAME)
+        poReader = readerPlugin.readers.values.first()
+
+        poReader?.let {
+
+            Timber.d("Initialize SEProxy with Android Plugin")
+
+            // define task as an observer for ReaderEvents
+            Timber.d("PO (NFC) reader name: ${it.name}")
+
+            it.setParameter("FLAG_READER_RESET_STATE", "0")
+            it.setParameter("FLAG_READER_PRESENCE_CHECK_DELAY", "100")
+            it.setParameter("FLAG_READER_NO_PLATFORM_SOUNDS", "0")
+            it.setParameter("FLAG_READER_SKIP_NDEF_CHECK", "0")
+
+            // with this protocol settings we activate the nfc for ISO1443_4 protocol
+            it.addSeProtocolSetting(
+                SeCommonProtocols.PROTOCOL_ISO14443_4,
+                AndroidNfcProtocolSettings.getSetting(SeCommonProtocols.PROTOCOL_ISO14443_4)
+            )
+            it.addSeProtocolSetting(
+                SeCommonProtocols.PROTOCOL_MIFARE_CLASSIC,
+                AndroidNfcProtocolSettings.getSetting(SeCommonProtocols.PROTOCOL_MIFARE_CLASSIC)
+            )
+        }
+
+        return poReader
+    }
+
+    @Throws(KeypleException::class)
+    override suspend fun initSamReaders(): Map<String, SeReader> {
+        return withContext(Dispatchers.IO) {
+            var readers: Map<String, SeReader>? = null
+            for (x in 1..MAX_TRIES) {
+                readers = SeProxyService.getInstance().getPlugin(PLUGIN_NAME).readers
+                if (readers == null || readers.size < 1) {
+                    Timber.d("No readers found in OMAPI Keyple Plugin")
+                    Timber.d("Retrying in 1 second")
+                    delay(1000)
+                } else {
+                    Timber.d("Readers Found")
+                    break
+                }
+            }
+
+            readers ?: throw KeyplePluginInstantiationException("No reader found")
+        }
+    }
+
+    override fun setSamParameters(samReader: SeReader) {
+        //Do nothing
+    }
+
+    override fun enableNfcReaderMode(activity: Activity) {
+        (poReader as AndroidNfcReader).enableNFCReaderMode(activity)
+    }
+
+    override fun disableNfcReaderMode(activity: Activity) {
+        (poReader as AndroidNfcReader).disableNFCReaderMode(activity)
+    }
+
+    override fun getSamReader(): SeReader? {
+        return if (samReaders.isNotEmpty()) {
+            samReaders.values.first()
+        } else {
+            null
+        }
+    }
+
+    override fun onDestroy() {
+        //Do nothing
+    }
+
+    companion object {
+        private const val MAX_TRIES = 10
+    }
+}

eclipse-keyple/keyple-util-java-lib

validator-app/src/famoco/kotlin/org/eclipse/keyple/famoco/validator/di/FamocoReaderRepositoryImpl.kt
@@ -1,0 +1,108 @@
+package org.eclipse.keyple.famoco.validator.di
+
+import android.app.Activity
+import org.eclipse.keyple.core.seproxy.SeProxyService
+import org.eclipse.keyple.core.seproxy.SeReader
+import org.eclipse.keyple.core.seproxy.exception.KeypleException
+import org.eclipse.keyple.core.seproxy.protocol.SeCommonProtocols
+import org.eclipse.keyple.famoco.se.plugin.AndroidFamocoPlugin
+import org.eclipse.keyple.famoco.se.plugin.AndroidFamocoPluginFactory
+import org.eclipse.keyple.famoco.se.plugin.AndroidFamocoReader
+import org.eclipse.keyple.famoco.validator.reader.IReaderRepository
+import org.eclipse.keyple.plugin.android.nfc.AndroidNfcPlugin
+import org.eclipse.keyple.plugin.android.nfc.AndroidNfcPluginFactory
+import org.eclipse.keyple.plugin.android.nfc.AndroidNfcProtocolSettings
+import org.eclipse.keyple.plugin.android.nfc.AndroidNfcReader
+import timber.log.Timber
+import javax.inject.Inject
+
+/**
+ *
+ *  created on 21/09/2020
+ *
+ *  @author youssefamrani
+ */
+
+class FamocoReaderRepositoryImpl @Inject constructor() :
+    IReaderRepository {
+
+    override var poReader: SeReader? = null
+    override var samReaders: MutableMap<String, SeReader> = mutableMapOf()
+
+    @Throws(KeypleException::class)
+    override fun registerPlugin() {
+        SeProxyService.getInstance().registerPlugin(AndroidNfcPluginFactory())
+        try {
+            SeProxyService.getInstance().registerPlugin(AndroidFamocoPluginFactory())
+        } catch (e: UnsatisfiedLinkError) {
+            Timber.w(e)
+        }
+    }
+
+    @Throws(KeypleException::class)
+    override suspend fun initPoReader(): SeReader? {
+        val readerPlugin = SeProxyService.getInstance().getPlugin(AndroidNfcPlugin.PLUGIN_NAME)
+        poReader = readerPlugin.readers.values.first()
+
+        poReader?.let {
+            Timber.d("Initialize SEProxy with Android Plugin")
+
+            // define task as an observer for ReaderEvents
+            Timber.d("PO (NFC) reader name: ${it.name}")
+
+            it.setParameter("FLAG_READER_RESET_STATE", "0")
+            it.setParameter("FLAG_READER_PRESENCE_CHECK_DELAY", "100")
+            it.setParameter("FLAG_READER_NO_PLATFORM_SOUNDS", "0")
+            it.setParameter("FLAG_READER_SKIP_NDEF_CHECK", "0")
+
+            // with this protocol settings we activate the nfc for ISO1443_4 protocol
+            it.addSeProtocolSetting(
+                SeCommonProtocols.PROTOCOL_ISO14443_4,
+                AndroidNfcProtocolSettings.getSetting(SeCommonProtocols.PROTOCOL_ISO14443_4)
+            )
+            it.addSeProtocolSetting(
+                SeCommonProtocols.PROTOCOL_MIFARE_CLASSIC,
+                AndroidNfcProtocolSettings.getSetting(SeCommonProtocols.PROTOCOL_MIFARE_CLASSIC)
+            )
+        }
+
+        return poReader
+    }
+
+    @Throws(KeypleException::class)
+    override suspend fun initSamReaders(): Map<String, SeReader> {
+        if (samReaders.isNullOrEmpty()) {
+            val samPlugin = SeProxyService.getInstance().getPlugin(AndroidFamocoPlugin.PLUGIN_NAME)
+
+            if (samPlugin != null) {
+                val samReader = samPlugin.getReader(AndroidFamocoReader.READER_NAME)
+
+                if (samReader != null) {
+                    samReaders[AndroidFamocoReader.READER_NAME] = samReader
+                }
+            }
+        }
+
+        return samReaders
+    }
+
+    override fun setSamParameters(samReader: SeReader) {
+        samReader.setParameter(AndroidFamocoReader.FLAG_READER_RESET_STATE, "")
+    }
+
+    override fun enableNfcReaderMode(activity: Activity) {
+        (poReader as AndroidNfcReader).enableNFCReaderMode(activity)
+    }
+
+    override fun disableNfcReaderMode(activity: Activity) {
+        (poReader as AndroidNfcReader).disableNFCReaderMode(activity)
+    }
+
+    override fun getSamReader(): SeReader? {
+        return samReaders[AndroidFamocoReader.READER_NAME]
+    }
+
+    override fun onDestroy() {
+        //Do nothing
+    }
+}

validator-app/src/main/kotlin/org/eclipse/keyple/famoco/validator/ticketing/AbstractTicketingSession.kt
@@ -11,15 +11,11 @@
  ********************************************************************************/
 package org.eclipse.keyple.famoco.validator.ticketing
 
+import org.eclipse.keyple.famoco.validator.reader.IReaderRepository
 import org.eclipse.keyple.calypso.command.sam.SamRevision
-import org.eclipse.keyple.calypso.transaction.CalypsoPo
-import org.eclipse.keyple.calypso.transaction.CalypsoSam
-import org.eclipse.keyple.calypso.transaction.ElementaryFile
-import org.eclipse.keyple.calypso.transaction.PoSecuritySettings
+import org.eclipse.keyple.calypso.transaction.*
 import org.eclipse.keyple.calypso.transaction.PoSecuritySettings.PoSecuritySettingsBuilder
 import org.eclipse.keyple.calypso.transaction.PoTransaction.SessionSetting.AccessLevel
-import org.eclipse.keyple.calypso.transaction.SamSelectionRequest
-import org.eclipse.keyple.calypso.transaction.SamSelector
 import org.eclipse.keyple.core.selection.SeResource
 import org.eclipse.keyple.core.selection.SeSelection
 import org.eclipse.keyple.core.selection.SelectionsResult
@@ -28,10 +24,12 @@ import org.eclipse.keyple.core.seproxy.SeReader
 import org.eclipse.keyple.core.seproxy.event.ObservableReader
 import org.eclipse.keyple.core.seproxy.exception.KeypleReaderException
 import org.eclipse.keyple.core.seproxy.protocol.SeCommonProtocols
-import org.eclipse.keyple.famoco.se.plugin.AndroidFamocoReader
 import timber.log.Timber
 
-abstract class AbstractTicketingSession protected constructor(val poReader: SeReader, protected val samReader: SeReader?) {
+abstract class AbstractTicketingSession protected constructor(
+    protected val readerRepository: IReaderRepository
+) {
+
     protected lateinit var calypsoPo: CalypsoPo
     protected lateinit var seSelection: SeSelection
     var poTypeName: String? = null
@@ -44,6 +42,7 @@ abstract class AbstractTicketingSession protected constructor(val poReader: SeRe
     protected lateinit var efEventLog: ElementaryFile
     protected lateinit var efCounter: ElementaryFile
     protected lateinit var efContractParser: ElementaryFile
+
     protected fun pad(text: String, c: Char, length: Int): String {
         val sb = StringBuffer(length)
         sb.append(text)
@@ -94,19 +93,20 @@ abstract class AbstractTicketingSession protected constructor(val poReader: SeRe
     }
 
     fun notifySeProcessed() {
-        (poReader as ObservableReader).finalizeSeProcessing()
+        (readerRepository.poReader as ObservableReader).finalizeSeProcessing()
     }
 
     @Throws(KeypleReaderException::class, IllegalStateException::class)
     protected fun checkSamAndOpenChannel(samReader: SeReader): SeResource<CalypsoSam> {
-        samReader.setParameter(AndroidFamocoReader.FLAG_READER_RESET_STATE, "")
+        readerRepository.setSamParameters(samReader)
         /*
          * check the availability of the SAM doing a ATR based selection, open its physical and
          * logical channels and keep it open
          */
         val samSelection = SeSelection(MultiSeRequestProcessing.FIRST_MATCH)
 
-        val samSelector = SamSelector.builder().seProtocol(SeCommonProtocols.PROTOCOL_ISO7816_3).samRevision(SamRevision.C1).build()
+        val samSelector = SamSelector.builder().seProtocol(SeCommonProtocols.PROTOCOL_ISO7816_3)
+            .samRevision(SamRevision.C1).build()
 
         samSelection.prepareSelection(SamSelectionRequest(samSelector))
 
@@ -157,5 +157,9 @@ abstract class AbstractTicketingSession protected constructor(val poReader: SeRe
                 DEFAULT_KEY_RECORD_NUMBER_DEBIT
             )
             .build()
+    }
+
+    fun samReaderAvailable(): Boolean {
+        return readerRepository.getSamReader() != null
     }
 }

validator-app/src/main/kotlin/org/eclipse/keyple/famoco/validator/util/Extensions.kt
@@ -1,0 +1,20 @@
+package org.eclipse.keyple.famoco.validator.util
+
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlinx.coroutines.withTimeoutOrNull
+import kotlin.coroutines.Continuation
+
+/**
+ *
+ *  created on 01/10/2020
+ *
+ *  @author youssefamrani
+ */
+
+suspend inline fun <T> suspendCoroutineWithTimeout(timeout: Long, crossinline block: (Continuation<T>) -> Unit ) : T? {
+    var finalValue : T? = null
+    withTimeoutOrNull(timeout) {
+        finalValue = suspendCancellableCoroutine(block = block)
+    }
+    return finalValue
+}

validator-app/src/omapi/kotlin/org/eclipse/keyple/famoco/validator/di/OmapiReaderRepositoryImpl.kt
@@ -1,0 +1,120 @@
+package org.eclipse.keyple.famoco.validator.di
+
+import android.app.Activity
+import android.content.Context
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
+import org.eclipse.keyple.core.seproxy.SeProxyService
+import org.eclipse.keyple.core.seproxy.SeReader
+import org.eclipse.keyple.core.seproxy.exception.KeypleException
+import org.eclipse.keyple.core.seproxy.exception.KeyplePluginInstantiationException
+import org.eclipse.keyple.core.seproxy.protocol.SeCommonProtocols
+import org.eclipse.keyple.famoco.validator.reader.IReaderRepository
+import org.eclipse.keyple.plugin.android.nfc.AndroidNfcPlugin
+import org.eclipse.keyple.plugin.android.nfc.AndroidNfcPluginFactory
+import org.eclipse.keyple.plugin.android.nfc.AndroidNfcProtocolSettings
+import org.eclipse.keyple.plugin.android.nfc.AndroidNfcReader
+import org.eclipse.keyple.plugin.android.omapi.AndroidOmapiPluginFactory
+import org.eclipse.keyple.plugin.android.omapi.PLUGIN_NAME
+import timber.log.Timber
+import javax.inject.Inject
+
+/**
+ *
+ *  created on 21/09/2020
+ *
+ *  @author youssefamrani
+ */
+
+class OmapiReaderRepositoryImpl @Inject constructor(private val applicationContext: Context) :
+    IReaderRepository {
+
+    override var poReader: SeReader? = null
+    override var samReaders: MutableMap<String, SeReader> = mutableMapOf()
+
+    @Throws(KeypleException::class)
+    override fun registerPlugin() {
+        SeProxyService.getInstance().registerPlugin(AndroidNfcPluginFactory())
+        SeProxyService.getInstance().registerPlugin(AndroidOmapiPluginFactory(applicationContext))
+    }
+
+    @Throws(KeypleException::class)
+    override suspend fun initPoReader(): SeReader? {
+        val readerPlugin = SeProxyService.getInstance().getPlugin(AndroidNfcPlugin.PLUGIN_NAME)
+        poReader = readerPlugin.readers.values.first()
+
+        poReader?.let {
+
+            Timber.d("Initialize SEProxy with Android Plugin")
+
+            // define task as an observer for ReaderEvents
+            Timber.d("PO (NFC) reader name: ${it.name}")
+
+            it.setParameter("FLAG_READER_RESET_STATE", "0")
+            it.setParameter("FLAG_READER_PRESENCE_CHECK_DELAY", "100")
+            it.setParameter("FLAG_READER_NO_PLATFORM_SOUNDS", "0")
+            it.setParameter("FLAG_READER_SKIP_NDEF_CHECK", "0")
+
+            // with this protocol settings we activate the nfc for ISO1443_4 protocol
+            it.addSeProtocolSetting(
+                SeCommonProtocols.PROTOCOL_ISO14443_4,
+                AndroidNfcProtocolSettings.getSetting(SeCommonProtocols.PROTOCOL_ISO14443_4)
+            )
+            it.addSeProtocolSetting(
+                SeCommonProtocols.PROTOCOL_MIFARE_CLASSIC,
+                AndroidNfcProtocolSettings.getSetting(SeCommonProtocols.PROTOCOL_MIFARE_CLASSIC)
+            )
+        }
+
+        return poReader
+    }
+
+    @Throws(KeypleException::class)
+    override suspend fun initSamReaders(): Map<String, SeReader> {
+        return withContext(Dispatchers.IO) {
+            var readers: Map<String, SeReader>? = null
+            for (x in 1..MAX_TRIES) {
+                readers = SeProxyService.getInstance().getPlugin(PLUGIN_NAME).readers
+                if (readers == null || readers.size < 1) {
+                    Timber.d("No readers found in OMAPI Keyple Plugin")
+                    Timber.d("Retrying in 1 second")
+                    delay(1000)
+                } else {
+                    Timber.d("Readers Found")
+                    break
+                }
+            }
+
+            readers ?: throw KeyplePluginInstantiationException("No reader found")
+        }
+    }
+
+    override fun setSamParameters(samReader: SeReader) {
+        //Do nothing
+    }
+
+    override fun enableNfcReaderMode(activity: Activity) {
+        (poReader as AndroidNfcReader).enableNFCReaderMode(activity)
+    }
+
+    override fun disableNfcReaderMode(activity: Activity) {
+        (poReader as AndroidNfcReader).disableNFCReaderMode(activity)
+    }
+
+    override fun getSamReader(): SeReader? {
+        return if (samReaders.isNotEmpty()) {
+            samReaders.values.first()
+        } else {
+            null
+        }
+    }
+
+    override fun onDestroy() {
+        //Do nothing
+    }
+
+    companion object {
+        private const val MAX_TRIES = 10
+    }
+}