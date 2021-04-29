af424c7d3b2778f072c4f1b48b0242f94618c26c
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2021-04-29 15:16:31 +0200

    feat(KEYP-385): add ApduUtil.

---
+++ src/main/java/org/eclipse/keyple/core/util/ApduUtil.java
@@ -0,0 +1,129 @@
+/* **************************************************************************************
+ * Copyright (c) 2021 Calypso Networks Association https://www.calypsonet-asso.org/
+ *
+ * See the NOTICE file(s) distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License 2.0 which is available at http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ ************************************************************************************** */
+package org.eclipse.keyple.core.util;
+
+/**
+ * Utils to build APDUs and check case 4.
+ *
+ * @since 2.0
+ */
+public final class ApduUtil {
+
+  /**
+   * private<br>
+   * Constructor
+   */
+  private ApduUtil() {}
+
+  /**
+   * Builds an APDU request from its elements as defined by the ISO 7816 standard.
+   *
+   * @param cla The class byte.
+   * @param ins The instruction byte.
+   * @param p1 The parameter 1.
+   * @param p2 The parameter 2.
+   * @param dataIn The data field of the command (optional).
+   * @param le The maximum number of bytes expected in the data field of the response to the command
+   *     (optional).
+   * @return A byte array containing the resulting apdu command data.
+   * @since 2.0
+   */
+  public static byte[] build(byte cla, byte ins, byte p1, byte p2, byte[] dataIn, Byte le) {
+    byte[] apduCommand;
+
+    /* Buffer allocation */
+    apduCommand = allocateBuffer(dataIn, le);
+
+    /* Build APDU buffer from provided arguments */
+    apduCommand[0] = cla;
+    apduCommand[1] = ins;
+    apduCommand[2] = p1;
+    apduCommand[3] = p2;
+
+    /* ISO7618 case determination and Le management */
+    if (dataIn != null) {
+      /* append Lc and ingoing data */
+      apduCommand[4] = (byte) dataIn.length;
+      System.arraycopy(dataIn, 0, apduCommand, 5, dataIn.length);
+      if (le != null) {
+        apduCommand[apduCommand.length - 1] = le;
+      } else {
+        /* case3: ingoing data only, no Le */
+      }
+    } else {
+      if (le != null) {
+        /* case2: outgoing data only */
+        apduCommand[4] = le;
+      } else {
+        /* case1: no ingoing, no outgoing data, P3/Le = 0 */
+        apduCommand[4] = (byte) 0x00;
+      }
+    }
+    return apduCommand;
+  }
+
+  /**
+   * (private)<br>
+   * Returns a byte array having the expected length according the APDU construction rules.
+   *
+   * @param data Data array (could be null).
+   * @param le Expected outgoing length (could be null).
+   * @return A new byte array.
+   */
+  private static byte[] allocateBuffer(byte[] data, Byte le) {
+    int length = 4; // header
+    if (data == null && le == null) {
+      // case 1: 5-byte apdu, le=0
+      length += 1; // Le
+    } else {
+      if (data != null) {
+        length += data.length + 1; // Lc + data
+      }
+      if (le != null) {
+        length += 1; // Le
+      }
+    }
+    return new byte[length];
+  }
+
+  /**
+   * Indicates if the provided byte array contains a case4 APDU command.
+   *
+   * <p>The ISO7816 case for data in a command-response pair is determined from the provided
+   * arguments:
+   *
+   * <ul>
+   *   <li><code>dataIn &nbsp;= null, le &nbsp;= null</code>&nbsp;&nbsp;&rarr;&nbsp;&nbsp;case 1: no
+   *       command data, no response data expected.
+   *   <li><code>dataIn &nbsp;= null, le != null</code>&nbsp;&nbsp;&rarr;&nbsp;&nbsp;case 2: no
+   *       command data, expected response data.
+   *   <li><code>dataIn != null, le &nbsp;= null</code>&nbsp;&nbsp;&rarr;&nbsp;&nbsp;case 3: command
+   *       data, no response data expected.
+   *   <li><code>dataIn != null, le &nbsp;= 0&nbsp;&nbsp;&nbsp;</code>
+   *       &nbsp;&nbsp;&rarr;&nbsp;&nbsp;case 4: command data, expected response data.
+   * </ul>
+   *
+   * Only the indication for case 4 is retained in the end.<br>
+   * In this case (incoming and outgoing data for the card), Le is set to 0, letting the lower layer
+   * (see API plugin) take care of recovering the exact length of the outgoing data.
+   *
+   * @param apduCommand The apduCommand to check.
+   * @return true the APDU command is case 4.
+   * @since 2.0
+   */
+  public static boolean isCase4(byte[] apduCommand) {
+    if (apduCommand != null && apduCommand.length > 4) {
+      return apduCommand[4] == apduCommand.length - 6;
+    }
+    return false;
+  }
+}
---
+++ src/test/java/core/util/ApduUtilTest.java
@@ -0,0 +1,77 @@
+/* **************************************************************************************
+ * Copyright (c) 2021 Calypso Networks Association https://www.calypsonet-asso.org/
+ *
+ * See the NOTICE file(s) distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License 2.0 which is available at http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ ************************************************************************************** */
+package core.util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.eclipse.keyple.core.util.ApduUtil;
+import org.eclipse.keyple.core.util.ByteArrayUtil;
+import org.junit.Test;
+
+public class ApduUtilTest {
+  private static final byte CLA = (byte) 0x11;
+  private static final byte INS = (byte) 0x22;
+  private static final byte P1 = (byte) 0x33;
+  private static final byte P2 = (byte) 0x44;
+  private static final byte[] DATA_IN =
+      new byte[] {(byte) 0x12, (byte) 0x34, (byte) 0x56, (byte) 0x78};
+  private static final Byte LE = 3;
+
+  private static final byte[] CASE1 = ByteArrayUtil.fromHex("1122334400");
+  private static final byte[] CASE2 = ByteArrayUtil.fromHex("1122334403");
+  private static final byte[] CASE3 = ByteArrayUtil.fromHex("112233440412345678");
+  private static final byte[] CASE4 = ByteArrayUtil.fromHex("11223344041234567803");
+
+  @Test
+  public void build_whenDataInAndLeAreNull_shouldReturnCase1() {
+    byte[] apduCommand = ApduUtil.build(CLA, INS, P1, P2, null, null);
+    assertThat(apduCommand).isEqualTo(CASE1);
+  }
+
+  @Test
+  public void build_whenDataInIsNull_shouldReturnCase2() {
+    byte[] apduCommand = ApduUtil.build(CLA, INS, P1, P2, null, LE);
+    assertThat(apduCommand).isEqualTo(CASE2);
+  }
+
+  @Test
+  public void build_whenLeIsNull_shouldReturnCase3() {
+    byte[] apduCommand = ApduUtil.build(CLA, INS, P1, P2, DATA_IN, null);
+    assertThat(apduCommand).isEqualTo(CASE3);
+  }
+
+  @Test
+  public void build_whenDataInAndLeAreNotNull_shouldReturnCase4() {
+    byte[] apduCommand = ApduUtil.build(CLA, INS, P1, P2, DATA_IN, LE);
+    assertThat(apduCommand).isEqualTo(CASE4);
+  }
+
+  @Test
+  public void isCase4_whenCase1_shouldReturnFalse() {
+    assertThat(ApduUtil.isCase4(CASE1)).isFalse();
+  }
+
+  @Test
+  public void isCase4_whenCase2_shouldReturnFalse() {
+    assertThat(ApduUtil.isCase4(CASE2)).isFalse();
+  }
+
+  @Test
+  public void isCase4_whenCase3_shouldReturnFalse() {
+    assertThat(ApduUtil.isCase4(CASE3)).isFalse();
+  }
+
+  @Test
+  public void isCase4_whenCase4_shouldReturnFalse() {
+    assertThat(ApduUtil.isCase4(CASE4)).isTrue();
+  }
+}
